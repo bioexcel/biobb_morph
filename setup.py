8c58372a02020351561991711e45e2d064c9065e
Pau Andrio <andriopau@gmail.com>
2024-12-17 15:24:38 +0100

    [CI/CD](setup.py): Addding setup tools installation

--- setup.py
+++ setup.py
@@ -5,35 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_morph",
-    version="4.1.0",
+    version="5.0.0",
     author="Biobb developers",
     author_email="mferri@bsc.es",
-    description="Biobb_Morph is a wrapped version of a Python- and Abaqus-based code for producing patien-specific 3D meshes from IVD template examples.",
+    description="biobb_morph is the Biobb module collection to create patien-specific 3D meshes from IVD template examples.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="Bioinformatics Workflows BioExcel Compatibility",
-    url="https://github.com/bioexcel/biobb_template",
+    url="https://github.com/bioexcel/biobb_morph",
     project_urls={
-        "Documentation": "http://biobb-template.readthedocs.io/en/latest/",
-        "Bioexcel": "https://bioexcel.eu/"
+        "Documentation": "http://biobb-morph.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/",
     },
-    packages=setuptools.find_packages(exclude=['docs']),
-    package_data={'biobb_morph': ['py.typed']},
-    install_requires=['biobb_common==4.1.0'],
-    python_requires='>=3.8',
-    entry_points={
-        "console_scripts": [
-            "template = biobb_morph.biobb_morph.morph:main"
-        ]
-    },
-    classifiers=(
+    packages=setuptools.find_packages(exclude=["docs", "test"]),
+    package_data={"biobb_morph": ["py.typed"]},
+    install_requires=["biobb_common==5.0.0", "torch"],
+    python_requires=">=3.9",
+    entry_points={"console_scripts": ["morph = biobb_morph.biobb_morph.morph:main"]},
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
-        "Operating System :: Unix"
-    ),
+        "Operating System :: Unix",
+    ],
 )
